f19743bce0816f6f8e219c2eb3e895fca21ab940
junindosite <juarezjunin2005@gmail.com>
2026-03-24 13:02:57 -0300

    feat: criação da entidade Produto

---
+++ src/main/java/com/junior/crud/entity/Produto.java
@@ -0,0 +1,50 @@
+package com.junior.crud.entity;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+    private BigDecimal preco;
+    private Integer estoque;
+
+    public Produto() {}
+
+    public long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public Integer getEstoque() {
+        return estoque;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public void setEstoque(Integer estoque) {
+        this.estoque = estoque;
+    }
+}
